a6a1e1d517bda1d9858087cb1445127eecb729f1
Eli Sacks <bastlifa@gmail.com>
2020-04-01 15:41:27 -0400

    event inputs, event type

--- app.js
+++ app.js
@@ -6,16 +6,33 @@ const { buildSchema } = require('graphql')
 
 const app = express()
 
+const events = []
+
 app.use(bodyParser.json())
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -25,14 +42,22 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return ['Romantic Cooking', 'Sailing', 'All-Night Coding']
+            return events
         },
         createEvent: (args) => {
-            const eventName = args.name
-            return eventName + " test"
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            }
+            events.push(event)
+            return event
         }
     },
     graphiql: true
 }))
 
-app.listen(5000)
+const port = process.env.PORT || 5000
+app.listen(port, _ => console.log(`server listening on port ${port}`))
